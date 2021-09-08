cacd8ad70d4cf0a03d0c2763f6bb89d1363c10a4
amitgalor18 <46008959+amitgalor18@users.noreply.github.com>
2021-09-08 09:58:34 +0300

    changed default MOT17 train set to MOT16. gets bad mota when running track.py with downloaded weights

--- track.py
+++ track.py
@@ -187,15 +187,25 @@ if __name__ == '__main__':
     print(opt, end='\n\n')
  
     if not opt.test_mot16:
-        seqs_str = '''MOT17-02-SDP
-                      MOT17-04-SDP
-                      MOT17-05-SDP
-                      MOT17-09-SDP
-                      MOT17-10-SDP
-                      MOT17-11-SDP
-                      MOT17-13-SDP
-                    '''
-        data_root = '/mnt/c/Users/amitg/Documents/TAU/Thesis/datasets/MOT/MOT17/images/train'
+        # seqs_str = '''MOT17-02-SDP
+        #               MOT17-04-SDP
+        #               MOT17-05-SDP
+        #               MOT17-09-SDP
+        #               MOT17-10-SDP
+        #               MOT17-11-SDP
+        #               MOT17-13-SDP
+        #             '''
+        # data_root = '/mnt/c/Users/amitg/Documents/TAU/Thesis/datasets/MOT/MOT17/images/train'
+        seqs_str = '''MOT16-02
+                              MOT16-04
+                              MOT16-05
+                              MOT16-09
+                              MOT16-10
+                              MOT16-11
+                              MOT16-13
+                            '''
+        data_root = '/mnt/c/Users/amitg/Documents/TAU/Thesis/datasets/MOT/MOT16/train'
+
     else:
         seqs_str = '''MOT16-01
                      MOT16-03
